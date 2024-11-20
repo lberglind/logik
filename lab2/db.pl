c5ff2f449a3502602e72473edee66b4b086118f5
Ludwig <luddeb03@gmail.com>
2024-11-20 01:52:21 +0100

    add comments

--- lab2/db.pl
+++ lab2/db.pl
@@ -1,20 +1,4 @@
-% En lista av premisser (vänsta delen av sekventen)
-%[neg(neg(imp(p, neg(p))))].
-
-% Målet (högra delen av sekventen).
-%neg(p).
-
-% Beviset
-%[
-%    [1, neg(neg(imp(p,neg(p)))),        premise     ],
-%    [2, imp(p, neg(p)),                 negnegel(1) ],
-%    [
-%        [3, p,                          assumption  ],
-%        [4, neg(p),                     impel(3,2)  ],
-%        [5, cont,                       negel(3,4)  ]
-%    ],
-%    [6, neg(p),                         negint(3,5) ]
-%].
+% Read input file, and check the proof
 
 verify(InputFileName) :- 
     see(InputFileName),
@@ -22,75 +6,73 @@ verify(InputFileName) :-
     seen,
     valid_proof(Prems, Goal, Proof).
 
-valid_proof(Prems, Goal, Proof) :-
+validProof(Prems, Goal, Proof) :-
     last(Proof, Goal),
-    check_steps(Prems, Goal, Proof, Proof).
+    checkSteps(Prems, Goal, Proof, Proof).
 
-check_steps(_, _, [], _).
+% Base step for 
+checkSteps(_, _, [], _).
 
-%check_steps(Prems, Goal, [Line|[]], Proof) :-
-%    (checkPremise(Line, Prems) ;
-%    checkRule(Line, Proof) ;
-%    box(Prems, Line, Proof)),
-%    Line == [_, Goal, _].
+% Recursively go through every row of the proof from top to bottom and check what Rule is used
+% and if it is correct.
 
-check_steps(Prems, Goal, [Line|Rest], Proof) :-
+checkSteps(Prems, Goal, [Line|Rest], Proof) :-
     (checkPremise(Line, Prems) ;
     checkRule(Line, Proof) ;
-    box(Prems, Line, Proof)),
-    check_steps(Prems, Goal, Rest, Proof).
+    newBox(Prems, Line, Proof)),
+    checkSteps(Prems, Goal, Rest, Proof).
 
 
 
-% Premise rule
+% Premise rule, premise
 checkPremise([_, X, premise], Prems) :-
     member(X, Prems).
 
-% And introduction rule
+% And introduction, andint
 checkRule([LineNum, and(X, Y), andint(A, B)], Proof) :-
     checkLines(LineNum, A, B),
     member([A, X, _], Proof),
     member([B, Y, _], Proof).
 
-% Or introduction 1
+% Or introduction 1, orint1
 checkRule([LineNum, or(X, _), orint1(A)], Proof) :-
     LineNum > A,
     member([A, X, _], Proof).
 
-% Or introduction 2
+% Or introduction 2, orint2
 checkRule([LineNum, or(_, Y), orint2(B)], Proof) :-
     LineNum > B,
     member([B, Y, _], Proof).
 
-% Implication introduction
+% Implication introduction, impint
 checkRule([LineNum, imp(X, Y), impint(A, B)], Proof) :-
     checkLines(LineNum, A, B),
     member([[A, X, assumption]|Box], Proof),
     boxLast([[A,X,_]|Box], Y, B).
 
-% Negation introduction
+% Negation introduction, negint
 checkRule([LineNum, neg(X), negint(A, B)], Proof) :-
     checkLines(LineNum, A, B),
     member([[A, X, assumption]|Box], Proof),
     boxLast([[A,X,_]|Box], cont, B).
 
-% And elimination 1
+% And elimination 1, andel1
 checkRule([LineNum, X, andel1(A)], Proof) :-
     checkLines(LineNum, A),
     member([A, and(X,_), _], Proof).
 
-% And elimination 2
+% And elimination 2, andel2
 checkRule([LineNum, Y, andel2(B)], Proof) :-
     checkLines(LineNum, B),
     member([B, and(_,Y), _], Proof).
 
-% Implication elimination
+% Implication elimination, impel
 checkRule([LineNum, X, impel(A, B)], Proof) :-
     checkLines(LineNum, A, B),
     member([A,Y,_], Proof),
     member([B, imp(Y,X), _], Proof).
 
-% Or elimination
+% Or elimination, orel
 checkRule([LineNum, X, orel(A, B, C, D, E)], Proof) :-
     checkLines(LineNum, A, B, C, D, E),
     member([A, or(U,V), _], Proof),
@@ -99,23 +81,23 @@ checkRule([LineNum, X, orel(A, B, C, D, E)], Proof) :-
     member([[D, V, assumption]|Box2], Proof),
     boxLast([[D,V,_]|Box2], X, E).
 
-% Negation elimination
+% Negation elimination, negel
 checkRule([LineNum, cont, negel(A, B)], Proof) :-
     checkLines(LineNum, A, B),
     member([A,X,_], Proof),
     member([B, neg(X), _], Proof).
 
-% Falsum elimination 
+% Falsum elimination, contel
 checkRule([LineNum, _, contel(A)], Proof) :-
     checkLines(LineNum, A),
     member([A, cont, _], Proof).
 
-% double negation introduction  negnegint
+% double negation introduction,  negnegint
 checkRule([LineNum, neg(neg(X)), negnegint(A)], Proof) :-
     checkLines(LineNum, A),
     member([A,X,_], Proof).
 
-% double negation elimination negnegel
+% double negation elimination, negnegel
 checkRule([LineNum, X, negnegel(A)], Proof) :-
     checkLines(LineNum, A),
     member([A, neg(neg(X)), _], Proof).
@@ -141,6 +123,8 @@ checkRule([LineNum, X, copy(A)], Proof) :-
 checkRule([_, or(X, neg(X)), lem],_).
 
 
+% Check so no rule calls for rows that are defined after the current row
+
 checkLines(Num, A) :-
     Num > A.
 checkLines(Num, A, B) :-
@@ -153,26 +137,14 @@ checkLines(Num, A, B, C, D, E) :-
     Num > D,
     Num > E.
 
-box(_, [Box], _) :-
-    Box = [_,_,assumption].
 
-box(Prems, [BoxStart|BoxTail], Proof) :-
+% Check box
+
+newBox(Prems, [BoxStart|BoxTail], Proof) :-
     BoxStart = [_ , _, assumption],
     append([BoxStart|BoxTail], Proof, ProofWBox),
-    check_steps(Prems, _, BoxTail, ProofWBox).
+    checkSteps(Prems, _, BoxTail, ProofWBox).
 
-
-
-
-%check_rule(_, LineNum, Formula, impel(LineA, LineB), Proof) :-
-%    formula_at(LineA, imp(Ant, Formula), Proof),
-%    formula_at(LineB, Ant, Proof).
-
-% Base case for checking last row of box
-boxLast([[RowB, VarY, _]], VarY, RowB).
-
-boxLast([_|T], VarY, RowB) :-
-    boxLast(T, VarY, RowB).
 
 % Traverse to last element and check if it is the same as Goal
 
@@ -181,3 +153,12 @@ last([[_, Goal, Rule]], Goal) :-
 
 last([_|Rest], Goal) :-
     last(Rest, Goal).
+
+
+% Base case for checking last row of box
+boxLast([[RowB, VarY, _]], VarY, RowB).
+
+boxLast([_|T], VarY, RowB) :-
+    boxLast(T, VarY, RowB).
+
+
